f019ceb38beb4f4e27bde7c75ef0758f01dcfb3c
Jawad Rashid <jawad.rashid98@gmail.com>
2020-12-29 20:04:17 +0000

    Initial commit

---
+++ schema/schema.js
@@ -0,0 +1,43 @@
+const graphql = require('graphql');
+
+const axios = require('axios');
+
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema
+} = graphql;
+
+const users = [
+    {id: '23', firstName: 'Bill', age: 20},
+    {id: '20', firstName: 'Sam', age: 21},
+    {id: '20', firstName: 'Bob', age: 20}
+];
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: {type: GraphQLString},
+        firstName: {type: GraphQLString},
+        age: {type: GraphQLInt}
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: {id: {type: GraphQLString}},
+            resolve(parentValue, args) {
+                return axios.get(`http:///localhost:3000/users/${args.id}`)
+                    .then(response => response.data);
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
